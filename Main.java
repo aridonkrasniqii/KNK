86a1a90d3366519a46822691743ebac2c5d5b95c
Aridon Krasniqi <77694113+aridonkrasniqii@users.noreply.github.com>
2022-03-25 20:41:34 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,163 +1,243 @@
 package application;
 	
-import javafx.application.Application;
-import javafx.geometry.Pos;
-import javafx.stage.Stage;
-import javafx.scene.Node;
-import javafx.scene.Scene;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Button;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
-import javafx.scene.control.TextField;
-import javafx.scene.control.Alert.AlertType;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
-
+import javafx.application.*;
+import javafx.collections.*;
+import javafx.geometry.*;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
 import java.util.Random;
 
-public class Main extends Application {
-	
+public class LoginRegisterForm extends Application {
 	
 	TextField usernameField;
-	PasswordField passwordField;
+	TextField passwordField;
 	@Override
 	public void start(Stage primaryStage) {
 		
-		primaryStage.setTitle("Application");
-	
 		TabPane tabPane = new TabPane();
+		Tab loginTab = new Tab("Login",new LoginForm());
+		Tab registerTab = new Tab("Register" , new RegisterForm());
 		
-		Tab loginTab = new Tab("Login",getLoginTab());
 		loginTab.setClosable(false);
-		Tab registerTab = new Tab("Register" , getRegisterTab());
 		registerTab.setClosable(false);
-		
 		tabPane.getTabs().addAll(loginTab,registerTab);
-		
-		
-		
-		
-		primaryStage.setScene(new Scene(tabPane,1000,700));
+	
+		primaryStage.setMinWidth(900);
+		primaryStage.setMinHeight(700);
+		primaryStage.setScene(new Scene(tabPane,900,700));
+		primaryStage.setTitle("Application");
 		primaryStage.show();	
 	}
 	
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
-	public Node getLoginTab() {
-		
-		GridPane pane = new GridPane();
-		pane.setHgap(15);
-		pane.setVgap(15);
-		
+}
 
-		Label username = new Label("Username: ");
-		username.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 20));
-		Label password = new Label("Password: ");
-		password.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 20));
-		
-		
-		usernameField = new TextField();
-		usernameField.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 20));
-		passwordField = new PasswordField();
-		passwordField.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 20));
-		
-		usernameField.setPromptText("Username..");
-		passwordField.setPromptText("Password..");
-		
-		
-		
-		// Css styling 
-		String css  = "-fx-background-color: darkslateblue; -fx-text-fill:white;";
-		
-		
-		Button submit = new Button("Submit");
-		submit.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 20));
-		submit.setPrefSize(100, 25);
-		submit.setStyle(css);
-		Button clear = new Button("Clear");
-		clear.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 20));
-		clear.setPrefSize(100,25);
-		clear.setStyle(css);
-		
-		
-		
-		// Event 
-		
-		submit.setOnAction(e -> {
-				Boolean result = validate();
-				if(result) { 
-					Alert alert = new Alert(AlertType.INFORMATION,"Data are  saved",ButtonType.OK);
-					clear();
-					alert.showAndWait();
-				}else {
-					Alert alert = new Alert(AlertType.ERROR,"Data are not saved",ButtonType.CANCEL);
-					alert.showAndWait();
-				}
-		});
-		
-		
-		clear.setOnAction(e -> {
-			clear();
-		});
-		
-		
-		
-		
-		
-		pane.add(username, 0, 0);
-		pane.add(usernameField,1, 0, 2 ,1 );
-		
-		pane.add(password, 0, 1);
-		pane.add(passwordField, 1, 1 ,2 ,1 );
-		
-		pane.add(submit, 1, 2);
-		pane.add(clear, 2, 2);
-		
-		
-		pane.setAlignment(Pos.CENTER);
-		
-		
-		
-		
-		return pane;
+class LoginForm extends GridPane {
+	
+	private TextField usernameField;
+	private PasswordField passwordField;
+	
+	
+	public LoginForm () {
+		super();
+		this.load();
 	}
 	
-	
-	public Node getRegisterTab() {
+	public LoginForm load() {
 		
-		GridPane pane = new GridPane();
-		
-		return pane;
-	}
-	
-	public boolean validate() {
-		
-		String username = usernameField.getText();
-		String password = passwordField.getText();
-		
-		if(!username.isEmpty() && !password.isEmpty()) {
-			return true;
+		try {
+
+			Label username = new Label("Username: ");
+			username.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 18));
+			Label password = new Label("Password: ");
+			password.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 18));
+			
+			usernameField = new TextField();
+			usernameField.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 18));
+			passwordField = new PasswordField();
+			passwordField.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 18));
+			usernameField.setPromptText("Username..");
+			passwordField.setPromptText("Password..");
+			
+			String css  = "-fx-background-color: darkslateblue; -fx-text-fill:white;";
+			
+			Button login = new Button("Submit");
+			login.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 16));
+			login.setPrefSize(80, 25);
+			login.setStyle(css);
+			Button clear = new Button("Clear");
+			clear.setFont(Font.font("Arial",FontWeight.SEMI_BOLD, 16));
+			clear.setPrefSize(80,25);
+			clear.setStyle(css);
+			
+			HBox buttonPane = new HBox(10);
+			buttonPane.getChildren().addAll(login,clear);
+			
+			// TODO:		
+			login.setOnAction(e -> {
+
+			});
+			
+			// TODO:
+			clear.setOnAction(e -> {
+
+			});
+			
+			super.setVgap(10);
+			super.setHgap(10);
+			super.add(username, 0, 0);
+			super.add(usernameField,1, 0, 2 ,1 );
+			super.add(password, 0, 1);
+			super.add(passwordField, 1, 1 ,2 ,1 );
+			super.add(buttonPane,1,2);
+			super.setAlignment(Pos.CENTER);
+			
+		}catch(Exception e ) {
+			System.out.println("An error happened in login form " + e );
 		}
+		return this;
 		
-		return false;
 	}
 	
+	// TODO: Validation .... 
 	
-	public void clear() {
+	public void validate() {
 		
-		passwordField.setText("");
-		usernameField.setText("");
+	}
+}
+
+class RegisterForm extends GridPane {
+	
+	private TextField nameField;
+	private TextField emailField;
+	private PasswordField passwordField;
+	private TextField usernameField;
+	private PasswordField confirmPasswordField;
+	private ToggleGroup genderGroup;
+	private RadioButton female;
+	private RadioButton male;
+	private ComboBox<String>locationView;
+	private Button registerButton;
+	
+	public RegisterForm() {
+		super();
+		this.load();
 	}
 	
+	public RegisterForm load() {
+		
+		try {
+			
+			Label nameLabel = new Label("Name ");
+			Label usernameLabel = new Label("Username");
+			Label emailLabel = new Label("Email");
+			Label passwordLabel = new Label("Password");
+			Label confirmPasswordLabel = new Label("Confirm Password");
+			Label birthdayLabel = new Label("Birthday");
+			Label genderLabel = new Label("Gender");
+			Label locationLabel = new Label("Location");
+			
+			nameField = new TextField();			
+			usernameField = new TextField();
+			emailField = new TextField();
+			passwordField = new PasswordField();
+			confirmPasswordField = new PasswordField();
+			
+			DatePicker birthdayPicker = new DatePicker();
+			
+			female = new RadioButton("Female");
+			male = new RadioButton("Male");
+			
+			genderGroup = new ToggleGroup();
+			female.setToggleGroup(genderGroup);
+			male.setToggleGroup(genderGroup);
+			HBox genderPane = new HBox(10);
+			genderPane.getChildren().addAll(female,male);
+			
+			locationView = new ComboBox<String>();
+			ObservableList<String> locationList = FXCollections.observableArrayList("Albania","Kosovo","Germany","France","Hungary");
+			locationView.getItems().addAll(locationList);
+			
+			registerButton = new Button("Register");
+			registerButton.setAlignment(Pos.BOTTOM_CENTER);
+			
+			HBox registerPane = new HBox();
+			registerPane.getChildren().addAll(registerButton);
+			
+			super.add(nameLabel, 0, 0);
+			super.add(nameField,1,0,3,1);
+			super.add(usernameLabel, 0, 1);
+			super.add(usernameField, 1, 1,3,1);
+			super.add(emailLabel, 0, 2);
+			super.add(emailField, 1, 2, 3, 1);
+			super.add(passwordLabel, 0, 3);
+			super.add(passwordField, 1, 3, 3 , 1 );
+			super.add(confirmPasswordLabel, 0, 4);
+			super.add(confirmPasswordField, 1, 4 ,3,1);
+			super.add(birthdayLabel, 0, 5);
+			super.add(birthdayPicker, 1, 5, 3,1 );
+			super.add(genderLabel, 0, 6);
+			super.add(genderPane, 2, 6);
+			super.add(locationLabel, 0, 7);
+			super.add(locationView, 1, 7, 3, 1);
+			super.add(registerPane, 3, 8);
+			super.setHgap(10);
+			super.setVgap(15);
+			super.setAlignment(Pos.CENTER);
+			
+		
+			
+			// TODO: Validation 
+			String name = nameField.getText();
+			String username = usernameField.getText();
+			String password = passwordField.getText();
+			String confirmPassword = confirmPasswordField.getText();
+			String email = emailField.getText();
+				
+		}catch(Exception e ) {
+			
+			System.out.println("An error happend in register form " + e);
+			
+		}
+			
+		return this;
+	}
+	
+	public boolean validate(String name,String username,String password, String email) {
+	
+		return true;
+	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
