07e3f1f15ad501cccac6bd03c58106e1b61073b5
Aridon Krasniqi <aridonk16@outlook.com>
2022-04-23 20:47:38 +0200

    cc

---
+++ Database/InsertQueryBuilder.java
@@ -0,0 +1,45 @@
+package Database;
+
+public class InsertQueryBuilder extends AbstractQueryBuilder {
+
+  public static InsertQueryBuilder create(String tableName) {
+    return new InsertQueryBuilder(tableName);
+  }
+
+  private InsertQueryBuilder(String tableName) {
+    super.tableName = tableName;
+  }
+
+  @Override
+  public String getQuery() {
+    String fields = "", values = "";
+    String lastElement = (String) this.queryValues.get(this.queryValues.size() - 1)[0];
+
+    for (Object[] e : this.queryValues) {
+      boolean isLastElement = e[0] == lastElement;
+      fields += e[0];
+      values += "?";
+      if (!isLastElement) {
+        fields += ", ";
+        values += ", ";
+      }
+    }
+
+    return "INSERT INTO " + super.tableName
+        + " (" + fields + ") VALUES ("
+        + values + ")";
+  }
+
+  public String getTypes() {
+    return this.queryTypes;
+  }
+
+  public Object[] getValues() {
+    Object[] values = new Object[this.queryValues.size()];
+    for (int i = 0; i < values.length; i++) {
+      values[i] = this.queryValues.get(i)[1];
+    }
+    return values;
+  }
+
+}
---
+++ model/RegisterGuests.java
@@ -0,0 +1,87 @@
+package model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class RegisterGuests {
+  private int id;
+  private String first_name;
+  private String username;
+  private String email;
+  private String password;
+  private String birthdate;
+  private String gender;
+  private String location;
+
+  private RegisterGuests(int id, String first_name, String username, String email, String password, String birthdate,
+      String gender, String location) {
+    this.id = id;
+    this.first_name = first_name;
+    this.username = username;
+    this.email = email;
+    this.password = password;
+    this.birthdate = birthdate;
+    this.gender = gender;
+    this.location = location;
+  }
+
+  public static RegisterGuests fromResultSet(ResultSet res) throws SQLException {
+
+    if (res.next()) {
+      try {
+        int id = res.getInt("id");
+        String first_name = res.getString("first_name");
+        String username = res.getString("username");
+        String email = res.getString("email");
+        String password = res.getString("password");
+        String birthdate = res.getString("birthdate");
+        String gender = res.getString("gender");
+        String location = res.getString("location");
+        return new RegisterGuests(id, first_name, username, email, password, birthdate, gender, location);
+      } catch (SQLException e) {
+        System.out.println("RegisterGuests class exception method: fromResultSet : " + e);
+        return null;
+      }
+    } else {
+      return null;
+    }
+  }
+
+  public static RegisterGuests createGuest(String first_name, String username, String email, String password,
+      String birthdate, String gender, String location) {
+    return new RegisterGuests(0, first_name, username, email, password, birthdate, gender, location);
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public String getFirst_name() {
+    return first_name;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public String getBirthdate() {
+    return birthdate;
+  }
+
+  public String getLocation() {
+    return location;
+  }
+
+}
