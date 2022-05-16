e797dcdf094df7ebdd69744f672bb98bd03c7eca
Andi <andiaa2002@gmail.com>
2022-05-16 13:39:47 +0200

    cc

--- src/repositories/ServicesTypeRepository.java
+++ src/repositories/ServicesTypeRepository.java
@@ -60,23 +60,23 @@ public class ServicesTypeRepository {
     }
 
 
-    public static Service_Type update(Service_Type service) throws Exception {
-        UpdateQueryBuilder query = (UpdateQueryBuilder) UpdateQueryBuilder.create("services_type")
-                .add("id", service.getId(), "i")
-                .add("service_name", service.getService_name(), "s")
-                .add("price", (float) service.getPrice(), "f")
-                .add("quantity", service.getQuantity(), "i");
+    public static Service_Type update(Service_Type model) throws Exception {
 
+        String query = "update services_types set service_name = ?, price = ? , quantity = ? where id = ?";
 
-        connection.execute(query);
+        PreparedStatement stmt = connection.prepareStatement(query);
+        stmt.setString(1,model.getService_name());
+        stmt.setDouble(2,model.getPrice());
+        stmt.setInt(3, model.getQuantity());
+        stmt.setInt(4,model.getId());
 
-        Service_Type updatedService = find(service.getId());
-
-        if (updatedService != null) {
-            return updatedService;
+        int affectedRows = stmt.executeUpdate();
+        if(affectedRows != 1) {
+            throw new Exception("ERR_NO_ROW_CHANGE");
         }
-        ErrorPopupComponent.show("Service could not be updated");
-        return null;
+
+        return find(model.getId());
+
     }
 
 
@@ -101,74 +101,7 @@ public class ServicesTypeRepository {
         return service_types;
     }
 
-    // public static List<Service_Type> selectAll() throws Exception {
-    //   ArrayList<Service_Type> list = new ArrayList<>();
-    //   Connection conn = DBConnect.getConnection();
-    //   PreparedStatement stmt = conn.prepareStatement("SELECT * FROM services_type");
-    //   ResultSet res = stmt.executeQuery();
-    //   while (res.next()) {
-    //     list.add(parseFromRes(res));
-    //   }
-    //   return list;
-    // }
 
-    // public static Service_Type find(int id) throws Exception {
-    //   Connection conn = DBConnect.getConnection();
-    //   PreparedStatement stmt = conn.prepareStatement("SELECT * FROM services_type WHERE id = ?");
-    //   stmt.setInt(1, id);
-    //   ResultSet res = stmt.executeQuery();
-    //   if (!res.next())
-    //     return null;
-    //   return parseFromRes(res);
-    // }
-
-    // public static Service_Type insert(Service_Type model) throws Exception {
-    //   Connection conn = DBConnect.getConnection();
-
-    //   PreparedStatement stmt = conn
-    //       .prepareStatement("INSERT INTO services_type (service_name, price, quantity) VALUES (?, ?, ?)");
-    //   stmt.setString(1, model.getService_name());
-    //   stmt.setDouble(2, model.getPrice());
-    //   stmt.setInt(3, model.getQuantity());
-    //   stmt.executeUpdate();
-
-    //   ResultSet res = conn.prepareStatement("SELECT id FROM services_type ORDER BY id DESC LIMIT 1").executeQuery();
-    //   res.next();
-    //   int id = res.getInt("id");
-
-    //   return find(id);
-    // }
-
-    // public static Service_Type update(Service_Type model) throws Exception {
-    //   Connection conn = DBConnect.getConnection();
-
-    //   PreparedStatement stmt = conn
-    //       .prepareStatement("UPDATE services_type SET service_name = ?, price = ?, quantity = ?");
-
-    //   stmt.setString(1, model.getService_name());
-    //   stmt.setDouble(2, model.getPrice());
-    //   stmt.setInt(3, model.getQuantity());
-    //   stmt.executeUpdate();
-
-    //   return find(model.getId());
-    // }
-
-    // public static boolean remove(int id) throws Exception {
-    //   Connection conn = DBConnect.getConnection();
-
-    //   PreparedStatement stmt = conn.prepareStatement("DELETE FROM services_type WHERE id = ?");
-    //   stmt.setInt(1, id);
-    //   return stmt.executeUpdate() >= 1;
-    // }
-
-    // private static Service_Type parseFromRes(ResultSet res) throws Exception {
-    //   int id = res.getInt("id");
-    //   String service_name = res.getString("service_name");
-    //   double price = res.getDouble("price");
-    //   int quantity = res.getInt("quantity");
-
-    //   return new Service_Type(id, service_name, price, quantity);
-    // }
 
     // public static int getLastID() throws Exception {
     //   Connection conn = DBConnect.getConnection();
